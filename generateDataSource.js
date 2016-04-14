d049d27962b15a0c029a4e5431f33d61ad1e7c7c
fuksi <dinh.tran@metropolia.fi>
2016-04-14 23:15:33 +0300

    Include datasource conversion in gulp automation, add new datasource, notes

--- generateDataSource.js
+++ generateDataSource.js
@@ -1,22 +1,26 @@
 // Generatejson
-var fs = require("fs");
-var resultJson;
-xlsxj = require("xlsx-to-json");
-xlsxj({
-	input: "datasource.xlsx", 
-	output: "datasource.json"
-}, function(err, result) {
-    resultJson = result.filter(function(item) {
-        return item.id !== "";
+function main() {
+    var fs = require("fs");
+    var resultJson;
+    xlsxj = require("xlsx-to-json");
+    xlsxj({
+        input: "datasource.xlsx", 
+        output: "datasource.json"
+    }, function(err, result) {
+        resultJson = result.filter(function(item) {
+            return item.id !== "";
+        });
     });
-});
 
-setTimeout(function() {
-    // json = fs.readFileSync("datasource.json", "utf8");
-    fs.writeFileSync("datasource.js", "var dataSource = ", "utf8");
-    fs.appendFileSync("datasource.js",  JSON.stringify(resultJson), "utf8");
-    fs.appendFileSync("datasource.js",  ";", "utf8");
-    
-    // Then remove empty rows
-    
-}, 1000);
+    setTimeout(function() {
+        // json = fs.readFileSync("datasource.json", "utf8");
+        fs.writeFileSync("datasource.js", "var dataSource = ", "utf8");
+        fs.appendFileSync("datasource.js",  JSON.stringify(resultJson), "utf8");
+        fs.appendFileSync("datasource.js",  ";", "utf8");
+        
+        // Then remove empty rows
+        
+    }, 1000);    
+};
+
+module.exports = main;
